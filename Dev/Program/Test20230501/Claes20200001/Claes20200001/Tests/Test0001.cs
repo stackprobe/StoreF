fd3e4bdeeb08a39808f2a88658ebb607315d9235
stackprobe <stackprobes@gmail.com>
2023-05-01 12:46:02 +0900

    Voyager 1 is 23906700589.691 kilometers away from the Sun.

--- Dev/Program/Test20230501/Claes20200001/Claes20200001/Tests/Test0001.cs
+++ Dev/Program/Test20230501/Claes20200001/Claes20200001/Tests/Test0001.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Drawing;
 using Charlotte.Commons;
+using Charlotte.Drawings;
+using Charlotte.Utilities;
 
 namespace Charlotte.Tests
 {
@@ -11,7 +13,21 @@ namespace Charlotte.Tests
 	{
 		public void Test01()
 		{
-			// none
+			Canvas canvas = Canvas.LoadFromFile(@"C:\temp\yande.re_268454.jpg");
+
+			canvas = canvas.GetSubImage(new I4Rect(0, 160, 2560, 1440));
+			canvas = canvas.Expand(1920, 1080);
+
+			canvas.Save(SCommon.NextOutputPath() + ".png");
+
+			// ----
+
+			canvas = Canvas.LoadFromFile(@"C:\temp\yande.re_268455.jpg");
+
+			canvas = canvas.GetSubImage(new I4Rect(0, 0, 2560, 1440));
+			canvas = canvas.Expand(1920, 1080);
+
+			canvas.Save(SCommon.NextOutputPath() + ".png");
 		}
 	}
 }
