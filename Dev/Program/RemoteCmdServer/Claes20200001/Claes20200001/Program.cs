e39cb6832a0cd44c90d1dc905bd5dfc975e4ad98
stackprobe <stackprobes@gmail.com>
2023-05-02 14:48:01 +0900

    Voyager 1 is 23908283562.751 kilometers away from the Sun.

--- Dev/Program/RemoteCmdServer/Claes20200001/Claes20200001/Program.cs
+++ Dev/Program/RemoteCmdServer/Claes20200001/Claes20200001/Program.cs
@@ -40,8 +40,8 @@ namespace Charlotte
 #if DEBUG
 			// -- choose one --
 
-			//Main4(new ArgsReader(new string[] { "80", @"..\..\..\..\TestData\Batch", @"C:\temp" }));
-			Main4(new ArgsReader(new string[] { "/P", "80", @"..\..\..\..\TestData\Batch", @"C:\temp" }));
+			Main4(new ArgsReader(new string[] { "80", @"..\..\..\..\TestData\Batch", @"C:\temp" }));
+			//Main4(new ArgsReader(new string[] { "/P", "80", @"..\..\..\..\TestData\Batch", @"C:\temp" }));
 			//new Test0001().Test01();
 			//new Test0002().Test01();
 			//new Test0003().Test01();
@@ -61,7 +61,7 @@ namespace Charlotte
 			{
 				ProcMain.WriteLog(ex);
 
-				//MessageBox.Show("" + ex, ProcMain.APP_TITLE + " / エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				MessageBox.Show("" + ex, Path.GetFileNameWithoutExtension(ProcMain.SelfFile) + " / エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
 				//Console.WriteLine("Press ENTER key. (エラーによりプログラムを終了します)");
 				//Console.ReadLine();
@@ -74,26 +74,26 @@ namespace Charlotte
 			{
 				HTTPServer hs = new HTTPServer()
 				{
-					//PortNo = 80,
-					//Backlog = 300,
-					//ConnectMax = 100,
-					Interlude = () => !evStop.WaitOne(0) && !Console.KeyAvailable, // 停止イベント / キー押下
+					PortNo = 80,
+					Backlog = 300,
+					ConnectMax = 100,
+					Interlude = () => !evStop.WaitOne(0) && !Console.KeyAvailable, // 終了条件：停止イベント || キー押下
 					HTTPConnected = P_Connected,
 				};
 
-				//SockChannel.ThreadTimeoutMillis = 100;
+				SockChannel.ThreadTimeoutMillis = 100;
 
-				//HTTPServer.KeepAliveTimeoutMillis = 5000;
+				HTTPServer.KeepAliveTimeoutMillis = 5000;
 
-				//HTTPServerChannel.RequestTimeoutMillis = -1;
-				//HTTPServerChannel.ResponseTimeoutMillis = -1;
-				//HTTPServerChannel.FirstLineTimeoutMillis = 2000;
-				//HTTPServerChannel.IdleTimeoutMillis = 180000;
+				HTTPServerChannel.RequestTimeoutMillis = -1;
+				HTTPServerChannel.ResponseTimeoutMillis = -1;
+				HTTPServerChannel.FirstLineTimeoutMillis = 2000;
+				HTTPServerChannel.IdleTimeoutMillis = 180000;
 				HTTPServerChannel.BodySizeMax = 100000000000000; // 100 TB
 
-				//SockCommon.TimeWaitMonitor.CTR_ROT_SEC = 60;
-				//SockCommon.TimeWaitMonitor.COUNTER_NUM = 5;
-				//SockCommon.TimeWaitMonitor.COUNT_LIMIT = 10000;
+				SockCommon.TimeWaitMonitor.CTR_ROT_SEC = 60;
+				SockCommon.TimeWaitMonitor.COUNTER_NUM = 5;
+				SockCommon.TimeWaitMonitor.COUNT_LIMIT = 10000;
 
 				// サーバーの設定ここまで
 
@@ -147,19 +147,20 @@ namespace Charlotte
 		{
 			try
 			{
-				P_Connected2(channel);
+				P_Connected_Main(channel);
 			}
 			catch (Exception ex)
 			{
 				ProcMain.WriteLog("通信失敗：" + ex.Message);
 
 				channel.ResStatus = 404;
-				channel.ResBody = new byte[][] { Encoding.UTF8.GetBytes(ex.ToString()) };
 				channel.ResHeaderPairs.Add(new string[] { "Content-Type", "text/plain; charset=UTF-8" });
+				channel.ResBody = new byte[][] { Encoding.UTF8.GetBytes(ex.ToString()) };
+				channel.ResBodyLength = -1L;
 			}
 		}
 
-		private void P_Connected2(HTTPServerChannel channel)
+		private void P_Connected_Main(HTTPServerChannel channel)
 		{
 			ProcMain.WriteLog("Client: " + channel.Channel.Handler.RemoteEndPoint);
 
@@ -175,24 +176,28 @@ namespace Charlotte
 
 			if (command == "B") // バッチ実行
 			{
-				string relPath = SCommon.ToFairRelPath(commandAndParams[1], this.BatchDir.Length);
+				string relPath = SCommon.ToFairRelPath(commandAndParams[1], SCommon.ENCODING_SJIS.GetByteCount(this.BatchDir));
 				string path = Path.Combine(this.BatchDir, relPath);
 
 				if (!File.Exists(path))
 					throw new Exception("no file");
 
-				string[] batch = new string[] { "TIMEOUT 2 > NUL" }
+				string[] batch = SCommon.EMPTY_STRINGS
+					//.Concat(new string[] { "TIMEOUT 2 > NUL" })
 					.Concat(File.ReadAllLines(path, SCommon.ENCODING_SJIS))
+					//.Concat(new string[] { "TIMEOUT 2 > NUL" })
 					.ToArray();
 
 				SCommon.Batch(batch, Path.GetDirectoryName(path), SCommon.StartProcessWindowStyle_e.MINIMIZED);
 
-				channel.ResBody = new byte[][] { Encoding.ASCII.GetBytes("B-OK") };
+				channel.ResStatus = 200;
 				channel.ResHeaderPairs.Add(new string[] { "Content-Type", "text/plain; charset=US-ASCII" });
+				channel.ResBody = new byte[][] { Encoding.ASCII.GetBytes("BATCH-OK") };
+				channel.ResBodyLength = -1L;
 			}
 			else
 			{
-				string relPath = SCommon.ToFairRelPath(commandAndParams[1], this.StoreDir.Length);
+				string relPath = SCommon.ToFairRelPath(commandAndParams[1], SCommon.ENCODING_SJIS.GetByteCount(this.StoreDir));
 				string path = Path.Combine(this.StoreDir, relPath);
 
 				if (command == "D") // ダウンロード
@@ -200,23 +205,29 @@ namespace Charlotte
 					if (!File.Exists(path))
 						throw new Exception("no file");
 
-					channel.ResBody = E_ReadFile(path);
+					channel.ResStatus = 200;
 					channel.ResHeaderPairs.Add(new string[] { "Content-Type", "application/octet-stream" });
+					channel.ResBody = E_ReadFile(path);
+					channel.ResBodyLength = -1L;
 				}
 				else if (command == "U") // アップロード
 				{
 					SCommon.CreateDir(Path.GetDirectoryName(path));
 					channel.Body.ToFile(path);
 
-					channel.ResBody = new byte[][] { Encoding.ASCII.GetBytes("U-OK") };
+					channel.ResStatus = 200;
 					channel.ResHeaderPairs.Add(new string[] { "Content-Type", "text/plain; charset=US-ASCII" });
+					channel.ResBody = new byte[][] { Encoding.ASCII.GetBytes("UPLOAD-OK") };
+					channel.ResBodyLength = -1L;
 				}
 				else if (command == "K") // 削除
 				{
 					SCommon.DeletePath(path);
 
-					channel.ResBody = new byte[][] { Encoding.ASCII.GetBytes("K-OK") };
+					channel.ResStatus = 200;
 					channel.ResHeaderPairs.Add(new string[] { "Content-Type", "text/plain; charset=US-ASCII" });
+					channel.ResBody = new byte[][] { Encoding.ASCII.GetBytes("KILL-OK") };
+					channel.ResBodyLength = -1L;
 				}
 				else
 				{
@@ -226,14 +237,13 @@ namespace Charlotte
 
 			channel.ResHeaderPairs.Add(new string[] { "Server", "RemoteCmdServer" });
 
-			// ----
-
 			SockCommon.WriteLog(SockCommon.ErrorLevel_e.INFO, "RES-STATUS " + channel.ResStatus);
 
 			foreach (string[] pair in channel.ResHeaderPairs)
 				SockCommon.WriteLog(SockCommon.ErrorLevel_e.INFO, "RES-HEADER " + pair[0] + " = " + pair[1]);
 
 			SockCommon.WriteLog(SockCommon.ErrorLevel_e.INFO, "RES-BODY " + (channel.ResBody != null));
+			SockCommon.WriteLog(SockCommon.ErrorLevel_e.INFO, "RES-BODY-LENGTH " + channel.ResBodyLength);
 		}
 
 		private static IEnumerable<byte[]> E_ReadFile(string file)
@@ -242,7 +252,7 @@ namespace Charlotte
 
 			for (long offset = 0L; offset < fileSize; )
 			{
-				int readSize = (int)Math.Min(2000000, fileSize - offset);
+				int readSize = (int)Math.Min(fileSize - offset, (long)(512 * 1024));
 				byte[] buff = new byte[readSize];
 
 				using (FileStream reader = new FileStream(file, FileMode.Open, FileAccess.Read))
